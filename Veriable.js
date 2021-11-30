3f6e2836e3e17c1ae7c87d3364134b1154ac1a86
Bekir GULER <90568365+bekirglr@users.noreply.github.com>
2021-11-30 15:09:38 +0300

    Create Veriable.js
    Javascript değişkenleri ile ilgili örnekler verdim

---
+++ Veriable.js
@@ -0,0 +1,11 @@
+var name = 'Bekir';
+console.log(name);
+
+var age = 20;
+console.log(age+" "+"Yasinda");
+
+var job;
+job = 'Software'
+console.log("Meslegi"+" "+job);    //.js uzantılı dosyaya kaydedilmeli 
+
+//---------------------------------------------------
